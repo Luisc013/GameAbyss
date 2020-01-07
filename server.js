6b37d0c8b7b75873b3ccd6883fe2d178dce281a1
Luis Carmona <c.luis013@gmail.com>
2020-01-07 16:45:30 -0500

    added postgres in comments

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //load in environment variables
 if(process.env.NODE_ENV !== "production") {
-    require('dotenv').config()
+  require('dotenv').config()
 }
 
 //dependency requirements
@@ -13,26 +13,48 @@ const session = require('express-session')
 const methodOverride = require('method-override')
 const path = require('path')
 const initializePassport = require('./passport-config')
-const engine = require('ejs-locals');
-app.engine('ejs', engine);
-
-
+const engine = require('ejs-locals')
+app.engine('ejs', engine)
 
 initializePassport(
-  passport,
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
+passport,
+email => users.find(user => user.email === email),
+id => users.find(user => user.id === id)
 )
 
 //fake DB
+// const {Client} = require('pg')
+// const client = new Client ({
+//   user: "postgres",
+//   password: "postgres",
+//   host: "lcarmona",
+//   port: 5432,
+//   database: "postgres"
+// })
+
+// // this is the non tri-catch method
+// client.connect()
+// // promise that after success logs a quote. the first then calls the next then. these are all one line functions
+// .then(() => console.log("Connected Successfully"))
+// .then(() => client.query("select * from users"))
+// // console has a method called .table that takes an array, loops thru it and prints into columns
+// .then(results => console.table(results.row))
+// // catches error
+// .catch(e => console.log())
+// // important to always terminate this connection with finally, the cleanup
+// .finally(() => client.end())
+
+
+
+
 const users = []
 
-//name of secret key in dotenv file, random string of number for security
+// name of secret key in dotenv file, random string of number for security
 app.use(flash())
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
+secret: process.env.SESSION_SECRET,
+resave: false,
+saveUninitialized: false
 }))
 app.use(passport.initialize())
 app.use(passport.session())
@@ -50,73 +72,73 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    // use res.render to load up an ejs view file
-    res.render('home.ejs')
-  })
+// use res.render to load up an ejs view file
+res.render('home.ejs')
+})
 
 app.get('/indies', (req, res) => {
-    res.render('indie.ejs')
-  })
+res.render('indie.ejs')
+})
 
 app.get('/community', (req, res) => {
-    res.render('community.ejs')
-  })
+res.render('community.ejs')
+})
 
 app.get('/about', (req, res) => {
-    res.render('about.ejs')
-  })
+res.render('about.ejs')
+})
 
 app.get('/profile', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.name })
-  })
-  
-  app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-  })
+res.render('index.ejs', { name: req.user.name })
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+res.render('login.ejs')
+})
 
 app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/profile',
-    failureRedirect: '/login',
-    failureFlash: true
-  }))
+  successRedirect: '/profile',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
 
 app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
-  })
+  res.render('register.ejs')
+})
 
-  app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10)
-      users.push({
-        id: Date.now().toString(),
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-      })
-      res.redirect('/login')
-    } catch {
-      res.redirect('/register')
-    }
-  })
-
-  app.delete('/logout', (req, res) => {
-    req.logOut()
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
     res.redirect('/login')
-  })
+  } catch {
+    res.redirect('/register')
+  }
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
 
 //middle thatll text req,res and the next variable when we done with authentication
 function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
-    }
-    res.redirect('/login')
+  if (req.isAuthenticated()) {
+      return next()
+  }
+  res.redirect('/login')
 }
 
 function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect('/profile')
-    }
-    next()
+  if (req.isAuthenticated()) {
+      return res.redirect('/profile')
+  }
+  next()
 }
 
 module.exports = app

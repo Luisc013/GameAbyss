32ba8a5d1e1aced1d4be61182365f6a1041ae256
Luisc013 <you@example.com>
2020-01-10 04:18:59 -0500

    Was able to get login success message thru the use of connect flash and express sessions.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ if(process.env.NODE_ENV !== "production") {
 const express = require('express')
 const app = express()
 const passport = require('passport')
-const flash = require('express-flash')
+const flash = require('connect-flash')
 const session = require('express-session')
 const expressLayouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
@@ -30,6 +30,22 @@ const database = require('./config/database.js')
     console.error('Unable to connect to the database:', err);
   });
   
+  //Express session middleware
+  app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  }))
+  //Express flash middleware
+  app.use(flash())
+
+  //Global variables for middleware
+  app.use((req, res, next) =>{
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+  })
+  
   
   // body parser middleware
   app.use(bodyParser.json());
@@ -49,13 +65,6 @@ const database = require('./config/database.js')
   module.exports = app
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, console.log(`Server started on port ${PORT}`));
-  // // name of secret key in dotenv file, random string of number for security
-  // app.use(flash())
-  // app.use(session({
-  //   secret: process.env.SESSION_SECRET,
-  //   resave: false,
-  //   saveUninitialized: false
-  // }))
   // app.use(passport.initialize())
   // app.use(passport.session())
   // app.use(methodOverride('_method'))
